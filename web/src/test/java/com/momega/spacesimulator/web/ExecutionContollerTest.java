ea034349e37c58da02ed533a21a5fce522b00ced
Martin Vanek <mvanek@email.cz>
2016-01-22 18:46:01 +0100

    obsolete import removed

--- web/src/test/java/com/momega/spacesimulator/web/ExecutionContollerTest.java
+++ web/src/test/java/com/momega/spacesimulator/web/ExecutionContollerTest.java
@@ -14,7 +14,6 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
